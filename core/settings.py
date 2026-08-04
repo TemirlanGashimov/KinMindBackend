fd295e61014d8d8096abfdba6c2d17fde5288cc0
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-04 20:11:03 +0200

    change settings and middleware

--- core/settings.py
+++ core/settings.py
@@ -37,11 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'auth_app',
+    'board_app',
+    'task_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
