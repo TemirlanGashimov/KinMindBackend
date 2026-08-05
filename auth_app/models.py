08ea5d6ac3299ec9523ac709a0dffcbf146ff151
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-05 14:51:22 +0200

    added models

--- auth_app/models.py
+++ auth_app/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    fullname = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.fullname
