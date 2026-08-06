b0ddf959d3f5b84e98528d6f66b1f5bf397776a0
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-06 14:03:50 +0200

    add: models

--- board_app/models.py
+++ board_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Board(models.Model):
+    title = models.CharField(max_length=255)
+    members = models.ManyToManyField(User, related_name='member_boards', blank=True)
+    member_count = models.IntegerField(default=0)
+    ticket_count = models.IntegerField(default=0)
+    tasks_to_do_count = models.IntegerField(default=0)
+    tasks_high_prio_count = models.IntegerField(default=0)
+
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='owner_boards')
+
+    def __str__(self):
+        return self.title
