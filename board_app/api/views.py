e78831162c1cd55a7ef22f31d5d295205d1e7158
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-10 20:42:24 +0200

    added get patch and delete for Boards with status

--- board_app/api/views.py
+++ board_app/api/views.py
@@ -1,9 +1,10 @@
 from django.db.models import Q
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from board_app.models import Board
-from .serializers import BoardSerializer, BoardCreateSerializer
-
+from .serializers import BoardSerializer, BoardCreateSerializer, BoardUpdateSerializer, BoardDetailSerializer
 
 
 class BoardListCreateView(generics.ListCreateAPIView):
@@ -11,7 +12,7 @@ class BoardListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return Board.objects.filter(
-            Q(owner=self.request.user) | 
+            Q(owner=self.request.user) |
             Q(members=self.request.user)
         ).distinct()
 
@@ -23,3 +24,29 @@ class BoardListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         board = serializer.save(owner=self.request.user)
         board.members.add(self.request.user)
+
+
+class BoardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Board.objects.filter(
+            Q(owner=self.request.user) |
+            Q(members=self.request.user)
+        ).distinct()
+
+    def get_serializer_class(self):
+        if self.request.method == 'PATCH':
+            return BoardUpdateSerializer
+        return BoardDetailSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        board = self.get_object()
+
+        if board.owner != request.user:
+            return Response(
+                {'detail': 'You do not have permission to delete this board.'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        return super().destroy(request, *args, **kwargs)
