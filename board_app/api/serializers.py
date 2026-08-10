c1ad222ee0693333874bc3c795e81961229bd98d
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-10 20:42:53 +0200

    add: implement BoardMemberSerializer for user details in Board API

--- board_app/api/serializers.py
+++ board_app/api/serializers.py
@@ -1,5 +1,13 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from board_app.models import Board
+
+class BoardMemberSerializer(serializers.ModelSerializer):
+    fullname = serializers.CharField(source='profile.fullname')
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'fullname']
 
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +19,15 @@ class BoardCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
         fields = ['title', 'members']
+
+class BoardUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = ['title', 'members']
+
+class BoardDetailSerializer(serializers.ModelSerializer):
+    members = BoardMemberSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Board
+        fields = ['id', 'title', 'owner_id', 'members']
