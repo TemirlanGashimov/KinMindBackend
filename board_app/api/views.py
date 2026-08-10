c541d8c34f2e9f3232196141b7090bd789458e72
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-10 21:17:44 +0200

    added email check

--- board_app/api/views.py
+++ board_app/api/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
 from django.db.models import Q
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from board_app.models import Board
-from .serializers import BoardSerializer, BoardCreateSerializer, BoardUpdateSerializer, BoardDetailSerializer
+from .serializers import BoardSerializer, BoardCreateSerializer, BoardUpdateSerializer, BoardDetailSerializer, BoardMemberSerializer
 
 
 class BoardListCreateView(generics.ListCreateAPIView):
@@ -50,3 +51,26 @@ class BoardDetailView(generics.RetrieveUpdateDestroyAPIView):
             )
 
         return super().destroy(request, *args, **kwargs)
+
+
+class EmailCheckView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        email = request.query_params.get('email')
+
+        if not email:
+            return Response(
+                {'detail': 'Email parameter is required.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = User.objects.filter(email=email).first()
+
+        if not user:
+            return Response(
+                {'detail': 'Email not found.'},
+                status=status.HTTP_404_NOT_FOUND)
+
+        serializer = BoardMemberSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
