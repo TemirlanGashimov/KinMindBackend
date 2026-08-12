42f3a10b664f2b124ec44d8d20fc3702e40dc03e
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-12 18:59:35 +0200

    added new import and url with pk

--- task_app/api/urls.py
+++ task_app/api/urls.py
@@ -1,6 +1,7 @@
 from django .urls import path
-from .views import TaskCreateView
+from .views import TaskCreateView, TaskDetailView
 
 urlpatterns = [
     path('tasks/', TaskCreateView.as_view(), name='task-create'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(),name='task-detail'),
 ]
