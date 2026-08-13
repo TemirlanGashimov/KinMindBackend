3bde91a7f980b95bcd85a943c0ff69226dc62b0c
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-13 13:08:18 +0200

    feat: add ReviewingView URL to urlpatterns for task review listing

--- task_app/api/urls.py
+++ task_app/api/urls.py
@@ -1,9 +1,10 @@
 from django .urls import path
-from .views import TaskCreateView, TaskDetailView, AssignedToMeView
+from .views import TaskCreateView, TaskDetailView, AssignedToMeView, ReviewingView
 
 urlpatterns = [
     path('tasks/', TaskCreateView.as_view(), name='task-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(),name='task-detail'),
     path('tasks/assigned-to-me/', AssignedToMeView.as_view(), name='assigned-list'),
+    path('tasks/reviewing/', ReviewingView.as_view(),name='reviewing-list')
 ]
 
