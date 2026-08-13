17ee460b08c2ac6d45ba9b3c9d1b8ca797941018
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-13 14:58:00 +0200

    feat: add comment management URLs for task comments functionality

--- task_app/api/urls.py
+++ task_app/api/urls.py
@@ -1,10 +1,13 @@
 from django .urls import path
-from .views import TaskCreateView, TaskDetailView, AssignedToMeView, ReviewingView
+from .views import TaskCreateView, TaskDetailView, AssignedToMeView, ReviewingView, CommentListCreateAPIView, CommentDeleteView
 
 urlpatterns = [
     path('tasks/', TaskCreateView.as_view(), name='task-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(),name='task-detail'),
     path('tasks/assigned-to-me/', AssignedToMeView.as_view(), name='assigned-list'),
-    path('tasks/reviewing/', ReviewingView.as_view(),name='reviewing-list')
+    path('tasks/reviewing/', ReviewingView.as_view(),name='reviewing-list'),
+    path('tasks/<int:task_id>/comments/', CommentListCreateAPIView.as_view(), name='comment-list-create'),
+    path('tasks/<int:task_id>/comments/<int:comment_id>/',CommentDeleteView.as_view(),name='comment-delete'
+)
 ]
 
