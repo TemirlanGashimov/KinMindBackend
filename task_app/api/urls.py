11459f0a80da77853ec04349a2a1051072f9f998
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-13 12:54:55 +0200

    added url for assigned-to-me

--- task_app/api/urls.py
+++ task_app/api/urls.py
@@ -1,7 +1,9 @@
 from django .urls import path
-from .views import TaskCreateView, TaskDetailView
+from .views import TaskCreateView, TaskDetailView, AssignedToMeView
 
 urlpatterns = [
     path('tasks/', TaskCreateView.as_view(), name='task-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(),name='task-detail'),
+    path('tasks/assigned-to-me/', AssignedToMeView.as_view(), name='assigned-list'),
 ]
+
