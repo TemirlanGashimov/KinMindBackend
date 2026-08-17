c41a95ec4a3c21e1067ac3f3daf871ac399f7549
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-17 19:56:35 +0200

    fix: update SECRET_KEY to use environment variable for improved security

--- core/settings.py
+++ core/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t#d(me=(aksawej8=-!+@v@1t+f9%tmn6f2$%bbmyqzl_7^q0s'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
