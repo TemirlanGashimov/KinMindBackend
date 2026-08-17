4f88fa6c573a1339350d9bed2db6efefa9ca8b07
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-17 19:58:16 +0200

    feat: implement UserProfileAdmin for managing UserProfile model in admin interface

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """Admin interface for UserProfile model."""
+    list_display = ['fullname', 'get_email', 'get_username']
+    search_fields = ['fullname', 'user__email', 'user__username']
+    readonly_fields = ['user']
+
+    def get_email(self, obj):
+        """Display user email."""
+        return obj.user.email
+    get_email.short_description = 'Email'
+
+    def get_username(self, obj):
+        """Display username."""
+        return obj.user.username
+    get_username.short_description = 'Username'
